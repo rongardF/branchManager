3d93d281174b9bf320549603d81ba8b82dbbaa65
Ron <ronfreimann@gmail.com>
2022-08-03 15:14:56 +0300

    updated the list of setups that need to be ignored

--- ignore_setups.py
+++ ignore_setups.py
@@ -1,3 +1,25 @@
 ignore_setups=["main", \
 				"master", \
-				]
+				"master_setup", \
+				"AIR_6449_Burnin_72h", \
+				"AIR1281_B257_full_traffic", \
+				"AIR1281_busy_hour", \
+				"AIR1281_busy_hour_30pcs", \
+				"AIR1281_full_traffic", \
+				"AIR1281_full_traffic_forced_backoff", \
+				"AIR5322_000002", \
+				"AIR5322_fan_off_full_traffic", \
+				"AIR5322_fan_on_busy_hour", \
+				"AIR5322_fan_on_thermal_cycling", \
+				"AIR6419_000004", \
+				"Branch_AIR_6419_Burnin_probe", \
+				"dualband_8843_B2B66A", \
+				"Dualband8843_000010_temp", \
+				"Dualband8843_000012", \
+				"Oslo2279_000001", \
+				"Oslo4419_000003", \
+				"SM6705_000006", \
+				"SM6705_000007", \
+				"SM6701_000008", \
+				"SM6701_000009"
+				]	
